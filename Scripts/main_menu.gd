dea606b4e2362c7b4f4cbd742c4bc33ffd584222
Zack-Calderwood <zack.calderwood@hotmail.ca>
2025-01-28 15:37:04 -0500

    ui stuff
    created base main menu and settings menu, need to add more setting functionality still

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Prefabs/Game.tscn")
