0cb31a34290644e9cd588d7f97b6b132a7a0af6f
Eriki Enos <Erikjenos@hotmail.com>
2025-01-29 20:56:27 -0500

    animation

--- Prefabs/game.gd
+++ Prefabs/game.gd
@@ -3,14 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("Game running")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	get_tree().change_scene_to_file("res://Prefabs/Game.tscn")
-	pass # Replace with function body.
