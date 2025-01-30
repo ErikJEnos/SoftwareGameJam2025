658ea19a0d83f0f07507f20937c08c2ff95c510c
Zack-Calderwood <zack.calderwood@hotmail.ca>
2025-01-29 21:03:10 -0500

    game muisc and hp bar

--- Scripts/enemy_bullet.gd
+++ Scripts/enemy_bullet.gd
@@ -28,7 +28,7 @@ func fire(direction: Vector2) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		body
+		print("Hiting player")
 		if(body.CheckHurt()):
 			body.RemovePlayerHealth(damage)
 			queue_free()
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -8,4 +8,4 @@ func _on_settings_button_pressed() -> void:
 	
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Prefabs/animation_scene.tscn")
+	get_tree().change_scene_to_file("res://Prefabs/Game.tscn")
